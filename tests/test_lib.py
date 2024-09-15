fix: test for computation check

tests/test_lib.py
@@ -1,82 +1,89 @@
+import os
+import sys
+from pathlib import Path
 import pytest
+
+from zkstats.computation import computation_to_model
+from zkstats.core import create_dummy, verifier_define_calculation
+
+current_dir = Path(__file__).parent
+sys.path.append(str(current_dir.parent))
+
 from lib import extract_safe_computation, ExtractComputationFailure
 
-def test_extract_safe_computation_valid():
+
+def execute_computation(tmp_path: Path, computation_str: str):
+    data_shape = {"x": 7, "y": 7}
+    module_path = tmp_path / "test_module.py"
+    precal_witness_path = tmp_path / "precal_witness.json"
+    dummy_data_path = tmp_path / "dummy_data.json"
+    sel_dummy_data_path = tmp_path / "sel_dummy_data.json"
+    model_path = tmp_path / "model.onnx"
+
+    extracted_computation = extract_safe_computation(computation_str, str(module_path))
+    selected_columns, _, verifier_model = computation_to_model(
+        extracted_computation,
+        str(precal_witness_path),
+        data_shape,
+        isProver=True
+    )
+    create_dummy(data_shape, dummy_data_path)
+    verifier_define_calculation(dummy_data_path, selected_columns, sel_dummy_data_path, verifier_model, model_path)
+    return extracted_computation
+
+
+def test_extract_safe_computation_valid(tmp_path):
     valid_computation = """
 def computation(state: State, args: Args):
     x = args["x"]
     y = args["y"]
     return state.mean(x), state.mean(y)
     """
-    module_path = "test_module.py"
-    result = extract_safe_computation(valid_computation, module_path)
-    assert callable(result)
+    extracted_computation = execute_computation(tmp_path, valid_computation)
+    assert callable(extracted_computation)
 
-def test_extract_safe_computation_valid_no_type_hint():
+def test_extract_safe_computation_valid_no_type_hint(tmp_path: Path):
     valid_computation = """
 def computation(state, args):
     x = args["x"]
     y = args["y"]
     return state.mean(x), state.mean(y)
     """
-    module_path = "test_module.py"
-    result = extract_safe_computation(valid_computation, module_path)
-    assert callable(result)
+    extracted_computation = execute_computation(tmp_path, valid_computation)
+    assert callable(extracted_computation)
 
-def test_extract_safe_computation_import_statement():
+def test_extract_safe_computation_import_statement(tmp_path: Path):
     invalid_computation = """
 import os
 
 def computation(state, args):
     return os.getcwd()
     """
-    module_path = "test_module.py"
     with pytest.raises(ExtractComputationFailure):
-        extract_safe_computation(invalid_computation, module_path)
+        execute_computation(tmp_path, invalid_computation)
 
-def test_extract_safe_computation_exec():
+def test_extract_safe_computation_exec(tmp_path: Path):
     invalid_computation = """
 def computation(state, args):
     exec("print('This should not be allowed')")
     return 0
     """
-    module_path = "test_module.py"
-    with pytest.raises(ExtractComputationFailure):
-        extract_safe_computation(invalid_computation, module_path)
+    # NameError: name 'exec' is not defined
+    with pytest.raises(NameError, match="name 'exec' is not defined"):
+        execute_computation(tmp_path, invalid_computation)
 
-def test_extract_safe_computation_eval():
+def test_extract_safe_computation_eval(tmp_path: Path):
     invalid_computation = """
 def computation(state, args):
     return eval("__import__('os').system('ls')")
     """
-    module_path = "test_module.py"
-    with pytest.raises(ExtractComputationFailure):
-        extract_safe_computation(invalid_computation, module_path)
+    with pytest.raises(NameError, match="name 'eval' is not defined"):
+        execute_computation(tmp_path, invalid_computation)
 
-def test_extract_safe_computation_globals():
+def test_extract_safe_computation_globals(tmp_path: Path):
     invalid_computation = """
 def computation(state, args):
     return globals()['__builtins__']['__import__']('os').system('ls')
     """
-    module_path = "test_module.py"
-    with pytest.raises(ExtractComputationFailure):
-        extract_safe_computation(invalid_computation, module_path)
-
-def test_extract_safe_computation_allowed_functions():
-    valid_computation = """
-def computation(state, args):
-    import torch
-    return torch.tensor([1, 2, 3]).mean()
-    """
-    module_path = "test_module.py"
-    result = extract_safe_computation(valid_computation, module_path)
-    assert callable(result)
-
-def test_extract_safe_computation_disallowed_attribute():
-    invalid_computation = """
-def computation(state, args):
-    return args.__class__.__bases__[0].__subclasses__()
-    """
-    module_path = "test_module.py"
-    with pytest.raises(ExtractComputationFailure):
-        extract_safe_computation(invalid_computation, module_path)
+    with pytest.raises(NameError, match="name 'globals' is not defined"):
+        execute_computation(tmp_path, invalid_computation)